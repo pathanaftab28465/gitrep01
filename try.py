0790a384f3807b163f0d8260694df23efe8ad91c
pathanaftab28465 <68348496+pathanaftab28465@users.noreply.github.com>
2024-08-22 04:24:09 +0000

    demo try file

---
+++ try.py
@@ -0,0 +1,10 @@
+student=(1,2,3.3,"Aftab","Rahul")
+teacher=(5,7,8.45,6)
+student.count("Aftab")
+#for i in student:
+    #print((student) + (teacher))
+
+count=0
+while count<10:
+    print(count)
+    count+=2
